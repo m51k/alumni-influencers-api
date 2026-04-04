231093a621efc3b17b8da7b5020c2b626ece783c
m51k <mekail@gmail.com>
2026-04-05 03:44:21 +0530

    add .env loading

--- application/config/email.php
+++ application/config/email.php
@@ -3,8 +3,8 @@
 $config['protocol'] = 'smtp';
 $config['smtp_host'] = 'sandbox.smtp.mailtrap.io';
 $config['smtp_port'] = 2525;
-$config['smtp_user'] = getenv('SMTP_USER');
-$config['smtp_pass'] = getenv('SMTP_PASSWORD');
+$config['smtp_user'] = $_ENV['SMTP_USER'];
+$config['smtp_pass'] = $_ENV['SMTP_PASS'];
 $config['mailtype'] = 'text';
 $config['charset'] = 'utf-8';
 $config['crlf'] = "\r\n";
--- public/index.php
+++ public/index.php
@@ -312,4 +312,9 @@ switch (ENVIRONMENT)
  *
  * And away we go...
  */
+
+require_once __DIR__ . '/../vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
+$dotenv->load();
+
 require_once BASEPATH.'core/CodeIgniter.php';
